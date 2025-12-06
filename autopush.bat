f6023df2eb6e2ea7c5e77147e3ba0d8bbc46a9f5
SoulFlameAdmin <soulflame.mitko@gmail.com>
2025-12-06 09:27:24 +0200

    Auto update съб 06.12.2025 г.  9:27:24,00

--- autopush.bat
+++ autopush.bat
@@ -1,11 +1,9 @@
-
-
-
 @echo off
 setlocal enabledelayedexpansion
 
 set "REPO=E:\BBQ_SITE"
 set "BRANCH=main"
+set "HOOK_URL=https://api.vercel.com/v1/integrations/deploy/prj_nhuHgdGOTehZGMuabcMVzTPtvcJX/Ymb7x0wu0E"
 set "SITE=https://bbqcorner.vercel.app/"
 
 cd /d "%REPO%" || (echo [X] Repo path not found & pause & exit /b 1)
@@ -17,9 +15,18 @@ git commit -m "Auto update %date% %time%" || echo [i] No changes to commit
 git pull --rebase origin "%BRANCH%"
 git push origin "%BRANCH%" || (echo [X] Git push failed & pause & exit /b 1)
 
-REM --- Само отваряме сайта с cache-buster ---
-set "TS=%DATE%_%TIME%"
-start "" "%SITE%?v=%RANDOM%%RANDOM%"
+REM --- Trigger Vercel Deploy Hook (1 retry) ---
+echo [>] Trigger Vercel deploy...
+for /f %%C in ('curl -s -o NUL -w "%%{http_code}" -X POST "%HOOK_URL%"') do set CODE=%%C
+echo [i] Hook HTTP: !CODE!
+if not "!CODE!"=="200" (
+  echo [!] Retrying deploy hook in 3s...
+  timeout /t 3 >nul
+  for /f %%C in ('curl -s -o NUL -w "%%{http_code}" -X POST "%HOOK_URL%"') do set CODE=%%C
+  echo [i] Hook HTTP: !CODE!
+)
+
+REM --- Open site with cache-buster ---
+start "" "%SITE%"
 
 pause
-
